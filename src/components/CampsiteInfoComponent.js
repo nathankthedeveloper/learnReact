4db4fed4879d82a097fde727e21d9c931a83e24b
Nathan Keh <clashk942@gmail.com>
2021-11-16 15:27:21 -0500

    Presentational and Container Components.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,17 +43,15 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-            
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className='row'>
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
-                
             )
         }
-              return (
-              <div> </div>
-              );
+            return <div />
     }
 }
 

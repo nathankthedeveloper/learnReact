92fda32e6dfee05ee9e5e2b41dc36b3c8bcaf808
Nathan Keh <clashk942@gmail.com>
2021-11-16 16:01:16 -0500

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
-import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-            <Main />
-          </div>
-      );
-  };
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    };
 }
 
 export default App;

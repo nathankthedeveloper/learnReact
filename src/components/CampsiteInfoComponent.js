5719ec59795f1ecfc7842b6c0fe32fe559be9875
Nathan Keh <clashk942@gmail.com>
2021-11-30 07:07:44 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -40,8 +40,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values){
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render(){
@@ -126,7 +126,7 @@ function RenderCampsite({campsite}) {
         )
     }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -143,7 +143,7 @@ function RenderComments({comments}){
                              
                         )
                     })}
-                     <CommentForm />
+                     <CommentForm campsiteId = {campsiteId} addComment={addComment}/>
                 </div>
             )
         }
@@ -166,7 +166,11 @@ function CampsiteInfo(props) {
                 </div>
                     <div className='row'>
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments
+                             comments={props.comments}
+                             addComment={props.addComment}
+                             campsiteId={props.campsiteId}
+                         />
                     </div>
                 </div>
             )

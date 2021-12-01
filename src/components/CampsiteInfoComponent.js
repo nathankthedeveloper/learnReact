9d66bd21cf50ed0cdbc350ad5c8ed9899d8a0807
Nathan Keh <clashk942@gmail.com>
2021-12-01 07:31:29 -0500

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import Button from 'reactstrap/lib/Button';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Component } from 'react';
 import ModalBody from 'reactstrap/lib/ModalBody';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -150,6 +151,26 @@ function RenderComments({comments, addComment, campsiteId}){
     }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
         if (props.campsite) {
             return (
                 <div className="container">
